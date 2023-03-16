6c66fcad84c21d96cbf93fbe5d2e73d445676ec4
FarhanShahriar11 <117368153+FarhanShahriar11@users.noreply.github.com>
2023-03-16 23:52:59 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+const loadMeals= (searchMeals) =>{
+    const url=(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchMeals}`)
+    fetch(url)
+    .then(res=>res.json())
+    .then(data =>displayMeals(data.meals))
+}
+const displayMeals =meals=>{
+    const mealContainer=document.getElementById('meals-container')
+    mealContainer.innerHTML='';
+   meals.forEach(meal=>{
+     console.log(meal)
+
+     const mealDiv=document.createElement('div');
+     mealDiv.classList.add('col');
+     mealDiv.innerHTML=`
+         <div class="card h-100">
+         <img src="${meal.strMealThumb
+         }" class="card-img-top" alt="...">
+         <div class="card-body">
+           <h5 class="card-title">${meal.strMeal}</h5>
+           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+           <!-- Button trigger modal -->
+            <button onclick="loadMealDetails(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetail">
+              Details
+            </button>
+
+           </div>
+        </div>
+     `
+  mealContainer.appendChild(mealDiv)
+   })
+   
+}
+const searchMeals=()=>{
+    const searchText=document.getElementById('search-field').value;
+    //console.log(searchText)
+    loadMeals(searchText);
+
+}
+const loadMealDetails=(idMeal)=>{
+    const url=(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displayMealsDetails(data.meals[0]))
+
+
+}
+const displayMealsDetails=meal=>{
+    document.getElementById('mealDetailLabel').innerText=meal.strMeal;
+    const mealDetails=document.getElementById('mealsDetailsBody')
+    mealDetails.innerHTML=`
+      <img class="img-fluid" src="${meal.strMealThumb}">
+    `
+}
+searchMeals()
+loadMeals('');
